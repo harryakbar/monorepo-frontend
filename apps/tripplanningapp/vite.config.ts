c0f6534d91a0ca126b2c0ede369ee32c3bc53b97
harryakbarntuc <harry.akbar@ntucenterprise.sg>
2026-02-05 10:59:20 +0800

    fix build

--- apps/tripplanningapp/vite.config.ts
+++ apps/tripplanningapp/vite.config.ts
@@ -2,7 +2,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [
@@ -10,7 +10,7 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
-  ],
+  ] as any,
   resolve: {
     alias: {
       // Alias @ to the src directory
